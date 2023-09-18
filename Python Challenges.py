e042f0e60ed2a564b2b86f60c6e297224a0cda94
Findlay Duffy <findlayduffey2@gmail.com>
2023-09-18 14:35:42 +0100

    18/09/2023 part 3
    18/09/2023 part 3 changes

--- Python Challenges.py
+++ Python Challenges.py
@@ -17,28 +17,28 @@ char_to_dots = {
 def triarea(base, height):
     Area = base * height / 2
     print('The area is:', Area)
-print(triarea(5, 10))
+triarea(5, 10)
 
 def area(base, height):
     Area = base * height
     print('The area is:', Area)
-print(area(5, 10))
+area(5, 10)
 
 def circuit_power(voltage, current):
     calculated_power = voltage * current
     print('The calculated voltage', calculated_power)
-print(circuit_power(500, 4))
+circuit_power(500, 4)
 
 # ---Previous code.---
 #def sum_of_two(num1, num2):
     #total = num1, num2
     #print('The total:', total)
-#print(sum_of_two(5, 7))
+#sum_of_two(5, 7)
 
 def sum_of_two(num1, num2):
     total = num1, num2
     print('The total:', total)
-print(sum_of_two(5, 7))
+sum_of_two(5, 7)
 
 def divide_multiply(num1, num2):
     multiples_of_seven = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70]
@@ -49,7 +49,7 @@ def divide_multiply(num1, num2):
         print(num1 * num2)
     elif num1 and num2 in multiples_of_seven or multiples_of_eleven:
         print('Try again.')
-print(divide_multiply(4, 5))
+divide_multiply(4, 5)
     
 
 def convert_age_to_days():
@@ -61,9 +61,23 @@ convert_age_to_days()
 
 def encrypt(msg):
     cipher = ''
-    
+    for letter in msg:
+        if letter != '':
+            cipher += char_to_dots[letter] + ''
+        else:
+            cipher += ''
+    return cipher
 
 def Encode_Morse(msg):
-    morse = char_to_dots
-    msg = morse
-print(Encode_Morse('Hi, how are you?'))
+    result = encrypt(msg.upper())
+    print(result)
+Encode_Morse('Hi, how are you?')
+
+def profit_calculation(cost, sell, inventory):
+    sell_price = sell - cost
+    if sell - cost:
+        inventory - 1
+    print(sell_price)
+    print(inventory)
+profit_calculation(32.67, 45, 1200)
+
