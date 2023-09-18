2c1f4579d52e1d17b5f4a2d0a6d7cbee08cf0d63
Findlay Duffy <findlayduffey2@gmail.com>
2023-09-18 10:47:05 +0100

    18/09/2023-2
    Second change.

--- Python Challenges.py
+++ Python Challenges.py
@@ -1,5 +1,19 @@
 import datetime
 
+char_to_dots = {
+  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+  'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+  'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+  'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+  '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+  '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+  '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
+  ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
+  '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+}
+
+
 def triarea(base, height):
     Area = base * height / 2
     print('The area is:', Area)
@@ -26,14 +40,17 @@ def sum_of_two(num1, num2):
     print('The total:', total)
 print(sum_of_two(5, 7))
 
-#def divide_multiply(num1, num2):
-    #multiples_of_seven = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70]
-    #multiples_of_eleven = [11, 22, 33, 44, 55, 66, 77, 88, 99, 110]
-    #if num1 == multiples_of_seven:
-        #print(num1 / num2)
-    #if num2 == multiples_of_eleven:
-        #print(num1 * num2)
-#print(divide_multiply(4, 5))
+def divide_multiply(num1, num2):
+    multiples_of_seven = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70]
+    multiples_of_eleven = [11, 22, 33, 44, 55, 66, 77, 88, 99, 110]
+    if num1 in multiples_of_seven:
+        print(num1 / num2)
+    if num2 in multiples_of_eleven:
+        print(num1 * num2)
+    elif num1 and num2 in multiples_of_seven or multiples_of_eleven:
+        print('Try again.')
+print(divide_multiply(4, 5))
+    
 
 def convert_age_to_days():
     birthday = datetime.date(2005, 5, 6)
@@ -42,5 +59,11 @@ def convert_age_to_days():
     print(delta.days)
 convert_age_to_days()
 
-#def Encode_Morse(msg, morse):
+def encrypt(msg):
+    cipher = ''
     
+
+def Encode_Morse(msg):
+    morse = char_to_dots
+    msg = morse
+print(Encode_Morse('Hi, how are you?'))
